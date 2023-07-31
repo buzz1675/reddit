048d0feb63130378e522299ec1b1e71e647a1eed
buzz1675 <buzz1675@gmail.com>
2023-07-31 14:21:28 +0100

    add comment styles

--- src/Features/Comment/comment.jsx
+++ src/Features/Comment/comment.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import moment from "moment";
+import './comment.css'
 
 const Comment = (props) => {
   const { comment } = props;
 
   return (
     <div className="comment">
-      <div>
+      <div className="comment_data">
         <p>{comment.author}</p>
         {moment.unix(comment.created_utc).fromNow()}
       </div>
-      <p>{comment.body}</p>
+      <p className="comment_body">{comment.body}</p>
     </div>
   );
 };
